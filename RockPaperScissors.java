219c72084aa683e172d5af588207f09dc70a8440
Auyon Debnath <auyon.debnath@students.mq.edu.au>
2026-04-06 00:19:00 +1000

    Java practice project

---
+++ RockPaperScissors.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+
+public class RockPaperScissors {
+
+    public void playerInput(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1. Rock");
+        System.out.println("2. Paper");
+        System.out.println("3. Scissor");
+        System.out.println("choose the number you want as your choice");
+        
+        int choice = Integer.parseInt(Scanner.nextLine());
+
+    }
+    public static void main(String[] args) {
+        
+    }
+}
